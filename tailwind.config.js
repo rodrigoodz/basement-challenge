cccb38e56422868dc0e7a2c873cab4dab6e43339
Rodrigo Ortiz de Zarate <rodrigoodz@gmail.com>
2022-01-18 13:10:09 -0300

    añadido color 'gradientColor'

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,14 @@
 module.exports = {
-  purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
+  purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
   darkMode: false, // or 'media' or 'class'
   theme: {
+    colors: {
+      gradientColor: "#1D1D1D",
+    },
     extend: {},
   },
   variants: {
     extend: {},
   },
   plugins: [],
-}
+};
