58694789fa9fb1e5e9f96c529ed9ed6f9c2b6492
Rodrigo Ortiz de Zarate <rodrigoodz@gmail.com>
2022-01-18 22:08:57 -0300

    creada logica para agregar productos al localStorage

--- components/Main/AddToCart.tsx
+++ components/Main/AddToCart.tsx
@@ -4,8 +4,50 @@ import { motion } from "framer-motion";
 
 import addtocart from "../../public/addtocart.svg";
 
-const AddToCart: React.FC = () => {
-  const handleClick = () => {};
+import { product } from "../../product/types";
+
+interface cartProduct {
+  name: string;
+  quantity: number;
+  totalPrice: number;
+}
+
+const AddToCart: React.FC<product> = ({ name, price }) => {
+  const handleClick = () => {
+    const data = JSON.parse(localStorage.getItem("cart") as string) || [];
+
+    const findIndex = data.findIndex((d: cartProduct) => d.name === name);
+
+    let newData: cartProduct[] = [];
+
+    // me fijo el producto ya esta en el localStorage
+    if (findIndex !== -1) {
+      // si ya esta armo un nuevo array modificando la cantidad y totalPrice del producto en cuestion
+      newData = data.map((d: cartProduct) => {
+        if (d.name === name) {
+          return {
+            name: name,
+            quantity: d.quantity + 1,
+            totalPrice: (d.quantity + 1) * price,
+          };
+        } else {
+          return d;
+        }
+      });
+    } else {
+      // sino directamente lo agrego al localStorage
+      newData = [
+        ...data,
+        {
+          name: name,
+          quantity: 1,
+          totalPrice: price,
+        },
+      ];
+    }
+
+    localStorage.setItem("cart", JSON.stringify(newData));
+  };
 
   return (
     <motion.div
